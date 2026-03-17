9d52a68f6a570867e24eeca999e7ea487c5a2fa0
mihir <mihirnetam33@gmail.com>
2026-03-17 10:36:05 +0530

    given by is by default in assign task

--- src/pages/admin/dashboard/StaffTaskTable.jsx
+++ src/pages/admin/dashboard/StaffTaskTable.jsx
@@ -303,7 +303,7 @@ const loadStaffData = useCallback(async (page = 1, append = false) => {
                   Done on Time
                 </th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  On Time Score
+                  Work Done Score
                 </th>
               </tr>
             </thead>
